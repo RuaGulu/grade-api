baa3f8d765887283ee4e08de0eeb4b1f59eda3da
RuaGulu <56189742+RuaGulu@users.noreply.github.com>
2023-09-28 01:46:15 -0400

    Finished implementing Get Average Grade Feature.

--- src/main/java/use_case/GetAverageGradeUseCase.java
+++ src/main/java/use_case/GetAverageGradeUseCase.java
@@ -11,7 +11,25 @@ public final class GetAverageGradeUseCase {
     }
 
     public float getAverageGrade(String course) {
-        // TODO: Get average grade for all students in your team.
-        return 0.0f;
+        // Get average grade for all students in your team.
+        // Initialization
+        int totalGrade = 0;
+        float avgGrade = 0;
+        int num = 0;
+
+        // Get myTeam using gradeDB function getMyTeam()
+        Team myTeam = gradeDB.getMyTeam();
+        // This is a local String[] for members, and then find the length
+        String[] people = myTeam.getMembers();
+        int peoplenum = people.length;
+
+        // Find totalGrade
+        while (num < peoplenum) {
+            totalGrade += gradeDB.getGrade(people[num], course).getGrade();
+            num++;
+        }
+        // Calculate for avgGrade
+        avgGrade = totalGrade / num;
+        return avgGrade;
     }
 }
